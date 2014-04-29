7517a6154083697dacf4b5af5dfad65ed0c99173
Marcus Isaksson <mail@naknut.se>
2014-04-29 15:14:08 +0200

    Added Pebble stuff Fixed setup script

--- Setup Scripts/setup.sh
+++ Setup Scripts/setup.sh
@@ -14,13 +14,6 @@ echo "Installing Homebrew..."
 /usr/bin/ruby -e "$(curl -fsSL https://raw.github.com/gist/323731)"
 export PATH=/usr/local/bin:$PATH
 
-# Warn about PATH
-echo ""
-echo "Add the following to ~/.bash_profile to use Homebrew:"
-echo ""
-echo "# Homebrew path"
-echo "export PATH=/usr/local/bin:\$PATH"
-
 echo 'Homebrew installed.'
 
 echo 'Done!'
